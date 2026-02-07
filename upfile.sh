9d41e87e184ba5d7797ca785490e014a04cbfe03
PlatovD <platovdima6@gmail.com>
2026-02-07 15:34:54 +0300

    Added gateway microservice

---
+++ upfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# restart
+kubectl delete namespaces spring-microservice-ns > /dev/null
+
+# global things
+kubectl apply -f namespace/global-ns.yaml
+kubectl apply -f config-map/global-cm.yaml
+
+# extra functional for testing
+kubectl apply -f test/pod.yaml
+
+# config-server
+kubectl apply -f config-map/config-server-cm.yaml
+kubectl apply -f secrets/config-server-secrets.yaml
+kubectl apply -f servers/config-server.yaml
+
+sleep 3
+
+# gateway-server
+kubectl apply -f config-map/gateway-server-cm.yaml
+kubectl apply -f servers/gateway-server.yaml
+
